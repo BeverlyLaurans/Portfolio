559cd4d5fb90deb4879dd2200f4d154e301759f1
BeverlyLaurans <beverly.laurans@free.fr>
2023-07-06 16:38:52 +0200

    Composant BackToTop

---
+++ src/components/BackToTop/BackToTop.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import whiteArrow from "../../assets/icons/interactions/white-arrow.svg";
+import "../../styles/scss/components/BackToTop.scss";
+
+function BackToTop() {
+    const [ backToTopButton, setBackToTopButton ] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 750) {
+                setBackToTopButton(true)
+            } else {
+                setBackToTopButton(false)
+            }
+        })
+    }, [])
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" })
+    }
+
+    return (
+        <div>
+            {backToTopButton && (
+                <button className="scroll-to-top" onClick={scrollToTop}>
+                    <img 
+                        src={whiteArrow}
+                        alt="flèche pour remonter tout en haut de la page"    
+                    >
+                    </img>
+                </button>
+            )}
+        </div>
+    )
+
+}
+
+
+export default BackToTop;
