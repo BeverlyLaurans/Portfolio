a1b5b1d9c237a0f607654fc9b88126f29c8d7ce4
BeverlyLaurans <beverly.laurans@free.fr>
2023-07-28 14:59:40 +0200

    Ajout bloc formation

---
+++ src/components/Formation/Formation.jsx
@@ -0,0 +1,32 @@
+import "../../styles/scss/components/Formation.scss";
+
+function Formation() {
+
+  return (
+    <section className="formation-container">
+        <h2>Mes diplômes</h2>
+        <div className="formation">
+            <h3>DÉVELOPPEU(R/SE) WEB : en cours de finalisation</h3>
+            <p>OpenClassrooms - 2023</p>
+        </div>
+        <div className="formation">
+            <h3>MASTER WEBMARKETING</h3>
+            <p>ECV Digital à Nantes (44) - 2018</p>
+        </div>
+        <div className="formation">
+            <h3>LICENCE PROFESSIONNELLE TICARC : Community Manager</h3>
+            <p>IUT de la Roche-Sur-Yon (85) - 2016</p>
+        </div>
+        <div className="formation">
+            <h3>DUT TECHNIQUES DE COMMERCIALISATION</h3>
+            <p>IUT de Poitiers, site de Châtellerault (86) - 2015</p>
+        </div>
+        <div className="formation">
+            <h3>BACCALAURÉAT LITTÉRAIRE - Spécialité Mathématiques</h3>
+            <p>Lycée Marcelin Berthelot à Châtellerault (86) - 2013</p>
+        </div>
+    </section>
+  );
+}
+
+export default Formation;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Introduction from "../../components/Introduction/Introduction";
 import ProjectList from "../../components/ProjectList/ProjectList";
 import Skills from "../../components/Skills/Skills";
 import ContactForm from "../../components/ContactForm/ContactForm";
+import Formation from "../../components/Formation/Formation";
 
 function Home() {
   
@@ -30,6 +31,7 @@ function Home() {
       <Introduction />
       <ProjectList />
       <Skills />
+      <Formation />
       <ContactForm />
     </div>
   );
